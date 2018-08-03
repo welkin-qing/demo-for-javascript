6b11bf4408bba8234aa459b83c9f465497c88a7e
welkin <welkin.qing@foxmail.com>
2018-08-03 23:23:07 +0800

    Bom，Dom相关方法及属性

--- JS for Intermediate lesson/2-BOM，Dom相关方法和属性/作业-2/my.js
+++ JS for Intermediate lesson/2-BOM，Dom相关方法和属性/作业-2/my.js
@@ -24,3 +24,64 @@ function getPos(obj){
 
 		}
 
+
+function getClass(parent, tagName, className) {
+
+
+
+			var aEls = parent.getElementsByTagName(tagName);
+
+			var arr = [];
+
+
+
+			for(var i=0; i<aEls.length; i++) {
+
+				var aClassName = aEls[i].className.split(' ');
+
+				for(var j=0; j<aClassName.length; j++){
+
+					if(aClassName[j] == className){
+
+						arr.push(aEls[i]);
+
+						break;
+
+					}
+
+				}
+
+			}
+
+			return arr;
+
+		}
+
+function getChecked(form, name){
+
+			var arr = [];
+
+			for(var i=0; i<form[name].length; i++){
+
+				if(form[name][i].checked){
+
+					arr.push(form[name][i].value);
+
+				}
+
+			}
+
+			if(form[name][0].type == 'radio'){
+
+				return arr[0];
+
+			}
+
+		if(form[name][0].type == 'checkbox'){
+
+				return arr;
+
+			}
+
+}
+
